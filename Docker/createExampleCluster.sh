aaf332b525a56a6c5592943e0e4f73c4ee27f4b6
Boris Glavic <lordpretzel@gmail.com>
2016-11-20 09:07:08 -0600

    stop postgres server

--- Docker/createExampleCluster.sh
+++ Docker/createExampleCluster.sh
@@ -7,16 +7,19 @@ DATADIR=${PERMHOME}/datadir
 ####################
 echo ---- Configure Perm installation and create TestDatabase
 echo -- for user ${PGUSER}
-echo -Create cluster, DB user and testdb database
+echo - Create cluster, DB user and testdb database
 mkdir -p ${DATADIR}
 ${INSTALLBINDIR}/initdb -D ${DATADIR}
 ####################
-echo -start server
+echo - start server
 #${INSTALLBINDIR}/postgres -D ${DATADIR} -l ${DATADIR}/log.txt
 ${INSTALLBINDIR}/pg_ctl -w start
 ${INSTALLBINDIR}/psql -h localhost -p 5432 -U perm -d template1 -c 'CREATE LANGUAGE plpgsql' 
 ${INSTALLBINDIR}/psql -h localhost -p 5432 -U perm -d template1 -c '\i /home/perm/src/contrib/xml2/pgxml.sql'
 ####################
-echo -create user and testdb
+echo - create user and testdb
 ${INSTALLBINDIR}/createuser -s -l -U $PGUSER postgres 
 ${INSTALLBINDIR}/createdb -U $PGUSER testdb
+####################
+echo - shutdown server
+${INSTALLBINDIR}/pg_ctl -w stop
