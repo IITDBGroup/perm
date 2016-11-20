7f0946da8a2c3b2a18f368fbd890088e7a401601
Boris Glavic <lordpretzel@gmail.com>
2016-11-20 08:12:44 -0600

    create cluster

--- Docker/createExampleCluster.sh
+++ Docker/createExampleCluster.sh
@@ -10,11 +10,11 @@ echo -Create cluster, DB user and testdb database
 mkdir -p ${DATADIR}
 ${INSTALLBINDIR}/initdb -D ${DATADIR}
 ####################
-echo -start server
-${INSTALLBINDIR}/postgres -D ${DATADIR} -l ${DATADIR}/log.txt &
-####################
 echo -create user and testdb
 ${INSTALLBINDIR}/createuser -s -l -U $PGUSER postgres 
-${INSTALLBINDIR}/psql -U postgres -c 'CREATE LANGUAGE plpgsql' template1
-${INSTALLBINDIR}/psql -U postgres -c '\i /home/perm/src/contrib/xml2/pgxml.sql' template1
+####################
+echo -start server
+${INSTALLBINDIR}/postgres -D ${DATADIR} & ${INSTALLBINDIR}/psql -U postgres -c 'CREATE LANGUAGE plpgsql' template1
+${INSTALLBINDIR}/postgres -D ${DATADIR} & ${INSTALLBINDIR}/psql -U postgres -c '\i /home/perm/src/contrib/xml2/pgxml.sql' template1
+####################
 ${INSTALLBINDIR}/createdb -U $PGUSER testdb
