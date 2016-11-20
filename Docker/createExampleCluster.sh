82e4c1d1536ac73836949b71870793deb6ff688a
Boris Glavic <lordpretzel@gmail.com>
2016-11-19 22:09:25 -0600

    readme.md

--- Docker/createExampleCluster.sh
+++ Docker/createExampleCluster.sh
@@ -12,4 +12,5 @@ perm/install/bin/postmaster -D /perm/datadir > /perm/datadir/log.txt 2>&1 &
 echo -create user and testdb
 /perm/install/bin/createuser -s -l -U $PGUSER postgres 
 /perm/install/bin/psql -U postgres -c 'CREATE LANGUAGE plpgsql' template1
+/perm/install/bin/psql -U postgres -c '\i /pathToPermCode/contrib/xml2/pgxml.sql' template1
 /perm/install/bin/createdb -U $PGUSER testdb
