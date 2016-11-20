fa2370e887612d9e884f11cd2b0f55008cad1732
Boris Glavic <lordpretzel@gmail.com>
2016-11-20 08:58:20 -0600

    create db script

--- Docker/createExampleCluster.sh
+++ Docker/createExampleCluster.sh
@@ -1,20 +1,22 @@
 #!/bin/bash
-echo ---- Configure Perm installation and create TestDatabase
 PGUSER=$1
 PERMHOME=/home/perm
 SRCDIR=${PERMHOME}/src
 INSTALLBINDIR=${PERMHOME}/install/bin
 DATADIR=${PERMHOME}/datadir
 ####################
+echo ---- Configure Perm installation and create TestDatabase
+echo -- for user ${PGUSER}
 echo -Create cluster, DB user and testdb database
 mkdir -p ${DATADIR}
 ${INSTALLBINDIR}/initdb -D ${DATADIR}
 ####################
+echo -start server
+#${INSTALLBINDIR}/postgres -D ${DATADIR} -l ${DATADIR}/log.txt
+${INSTALLBINDIR}/pg_ctl -w start
+${INSTALLBINDIR}/psql -h localhost -p 5432 -U perm -d template1 -c 'CREATE LANGUAGE plpgsql' 
+${INSTALLBINDIR}/psql -h localhost -p 5432 -U perm -d template1 -c '\i /home/perm/src/contrib/xml2/pgxml.sql'
+####################
 echo -create user and testdb
 ${INSTALLBINDIR}/createuser -s -l -U $PGUSER postgres 
-####################
-echo -start server
-${INSTALLBINDIR}/postgres -D ${DATADIR} & ${INSTALLBINDIR}/psql -U postgres -c 'CREATE LANGUAGE plpgsql' template1
-${INSTALLBINDIR}/postgres -D ${DATADIR} & ${INSTALLBINDIR}/psql -U postgres -c '\i /home/perm/src/contrib/xml2/pgxml.sql' template1
-####################
 ${INSTALLBINDIR}/createdb -U $PGUSER testdb
